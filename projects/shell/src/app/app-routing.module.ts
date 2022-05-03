c21ad4750c22e40344b55c210478b8be22ab7e12
Jason Marston <jason@jasonmarston.me.uk>
2022-05-03 20:56:49 +0100

    authentication

--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WebComponentWrapper, WebComponentWrapperOptions } from '@angular-architects/module-federation-tools';
-import { AutoLoginPartialRoutesWithRoleGuard, shellPath } from 'lib1';
+import { AutoLoginAllRoutesWithRoleGuard, AutoLoginPartialRoutesWithRoleGuard, shellPath } from 'lib1';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { UnauthorisedComponent } from './unauthorised/unauthorised.component';
-import { OidcSecurityService, AutoLoginPartialRoutesGuard, AutoLoginAllRoutesGuard } from 'angular-auth-oidc-client';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AutoLoginAllRoutesGuard] }, // unprotected entrypoint
-  { path: 'home', component: HomeComponent, canActivate: [AutoLoginAllRoutesGuard] },
+  { path: '', component: HomeComponent, canActivate: [AutoLoginAllRoutesWithRoleGuard]  },
+/*{ path: '', component: HomeComponent }, // unprotected entrypoint */
   {
     matcher: shellPath('component'),
     component: WebComponentWrapper,
--- projects/shell/src/app/app.component.ts
+++ projects/shell/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OidcSecurityService} from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shell';
-  constructor() {}
+  constructor(public oidcSecurityService: OidcSecurityService) {}
 
   ngOnInit() {
+    // uncomment the following if using AutoLoginPartialRoutesWithRoleGuard
+
+/*    this.oidcSecurityService.checkAuth().subscribe(({ isAuthenticated, userData, accessToken}) => {
+      console.log('Shell: app authenticated', isAuthenticated);
+      console.log(`Shell: Current access token is '${accessToken}'`);
+    });*/
   }
 }
