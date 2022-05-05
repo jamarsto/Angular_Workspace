592d9ae84be1b447f9023c55912563f1b88beec8
Jason Marston <jason@jasonmarston.me.uk>
2022-05-05 12:31:28 +0100

    simplified shell application to redirect into a chosen MFE instead of having local component

--- projects/app-shell/src/app/app-routing.module.ts
+++ projects/app-shell/src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AutoLoginAllRoutesWithRoleGuard, microFrontEndRoute, NotFoundComponent, UnauthorisedComponent } from 'lib-micro-front-end';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AutoLoginAllRoutesWithRoleGuard] },
+  { path: '', redirectTo: 'retail', pathMatch: 'full' },
   { path: 'unauthorized', component: UnauthorisedComponent },
-  microFrontEndRoute({ path: 'retail', name: 'app-app1', guards: [AutoLoginAllRoutesWithRoleGuard], roles: ['ADMIN'] }),
-  microFrontEndRoute({ path: 'business', name: 'app-app2', guards: [AutoLoginAllRoutesWithRoleGuard], roles: ['ADMIN'] }),
+  microFrontEndRoute({ path: 'retail', name: 'app-app1', guards: [AutoLoginAllRoutesWithRoleGuard], roles: ['ADMIN', 'USER'] }),
+  microFrontEndRoute({ path: 'business', name: 'app-app2', guards: [AutoLoginAllRoutesWithRoleGuard], roles: ['ADMIN', 'USER'] }),
   { path: '**', component: NotFoundComponent }
 ];
 
--- projects/app-shell/src/app/app.module.ts
+++ projects/app-shell/src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AuthConfigModule, MicroFrontEndModule } from 'lib-micro-front-end';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
   ],
   imports: [
     AppRoutingModule,
