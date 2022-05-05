245d45a41d4d8ffa92e390387c1e6ff42c999336
Jason Marston <jason@jasonmarston.me.uk>
2022-05-05 08:23:05 +0100

    cleanup

--- projects/lib-micro-front-end/src/lib/function/modulePath/module-path.function.ts
+++ projects/lib-micro-front-end/src/lib/function/modulePath/module-path.function.ts
@@ -2,10 +2,7 @@ import { UrlMatcher, UrlSegment } from '@angular/router';
 
 export function modulePath(): UrlMatcher {
   return (url: UrlSegment[]) => {
-    if(url.length === 1) {
-      return ({ consumed: url });
-    }
-    if(url.length > 1) {
+    if(url.length > 0) {
       return ({ consumed: [url[0]] });
     }
     return null;
--- projects/lib-micro-front-end/src/lib/function/shellPath/shell-path.function.ts
+++ projects/lib-micro-front-end/src/lib/function/shellPath/shell-path.function.ts
@@ -2,7 +2,7 @@ import { UrlMatcher, UrlSegment } from '@angular/router';
 
 export function shellPath(shellPath: string): UrlMatcher {
   return (url: UrlSegment[]) => {
-    if(url.length >= 1 && url[0].path === shellPath) {
+    if(url.length > 0 && url[0].path === shellPath) {
       return ({ consumed: url });
     }
     return null;
