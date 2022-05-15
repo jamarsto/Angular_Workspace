1af67d34a6473bf72061375c71e3e74b3d44456c
Jason Marston <jason@jasonmarston.me.uk>
2022-05-15 16:16:13 +0100

    reset home redirect

--- projects/app-shell/src/app/app-routing.module.ts
+++ projects/app-shell/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AutoLoginAllRoutesWithRoleGuard, microFrontEndRoute, NotFoundComponent, UnauthorisedComponent } from 'lib-micro-front-end';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'retail/path', pathMatch: 'full' },
+  { path: '', redirectTo: 'retail', pathMatch: 'full' },
   { path: 'unauthorized', component: UnauthorisedComponent },
   microFrontEndRoute({ path: 'retail', name: 'app-app1', guards: [AutoLoginAllRoutesWithRoleGuard], roles: ['ADMIN', 'USER'] }),
   microFrontEndRoute({ path: 'business', name: 'app-app2', guards: [AutoLoginAllRoutesWithRoleGuard], roles: ['ADMIN', 'USER'] }),
