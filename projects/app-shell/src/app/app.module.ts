012e554f187264869c45472bcc320e432a133265
Jason Marston <jason@jasonmarston.me.uk>
2022-05-16 17:09:36 +0100

    cleanup

--- projects/app-shell/src/app/app.module.ts
+++ projects/app-shell/src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from './local-app/header/header.component';
-import { FooterComponent } from './local-app/footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
